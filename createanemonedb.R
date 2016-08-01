6a1fa5cd941a36910cc8a66b7cb7ffedeafdd2b6
Michelle Stuart <bunx21@yahoo.com>
2016-08-01 15:04:37 -0400

    remove divenum and add numfish from createanemonedb.R

--- createanemonedb.R
+++ createanemonedb.R
@@ -6,7 +6,6 @@ leyte <- dbConnect(MySQL(), host="amphiprion.deenr.rutgers.edu", user="michelles
 dbSendQuery(leyte, "CREATE TABLE `anemones` (
   `anem_table_id` int(11) NOT NULL AUTO_INCREMENT,
   `dive_table_id` int(11) NOT NULL,
-  `DiveNum` int(11) NOT NULL,
   `ObsTime` time NOT NULL,
   `Collector` text NOT NULL,
   `GPS` text,
@@ -17,6 +16,8 @@ dbSendQuery(leyte, "CREATE TABLE `anemones` (
   `oldAnemID` text,
   `AnemID` text,
   `AnemSampleID` text,
+`NumFish` int,
   `Notes` text,
   PRIMARY KEY (`anem_table_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=20221 DEFAULT CHARSET=latin1;")
+
