26c9d27692181c6005d9cdbe5ce03d363a39a716
Michelle Stuart <bunx21@yahoo.com>
2016-08-01 21:44:21 -0400

    remove dive_id

--- createclownfishdb.R
+++ createclownfishdb.R
@@ -5,7 +5,6 @@ leyte <- dbConnect(MySQL(), host="amphiprion.deenr.rutgers.edu", user="michelles
 
 dbSendQuery(leyte, "CREATE TABLE `clownfish` (
   `fish_table_id` int(11) NOT NULL AUTO_INCREMENT,
-  `dive_id` int(11) DEFAULT NULL,
   `anem_table_id` bigint(20) DEFAULT NULL,
   `Spp` text,
   `Size` double DEFAULT NULL,
